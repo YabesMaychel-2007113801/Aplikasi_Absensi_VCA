9aec8e178ba2d95a7d80fda64c83ec7a748a1403
Alvien Cenna <alpiencenna@gmail.com>
2024-06-03 16:26:40 +0700

    feat: status waktu

--- app/src/main/java/com/tugas/platform/aplikasiabsensi/adapters/AbsensAdapter.kt
+++ app/src/main/java/com/tugas/platform/aplikasiabsensi/adapters/AbsensAdapter.kt
@@ -1,15 +1,19 @@
 package com.tugas.platform.aplikasiabsensi.adapters
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.tugas.platform.aplikasiabsensi.R
 import com.tugas.platform.aplikasiabsensi.databinding.ItemAbsenBinding
 import com.tugas.platform.aplikasiabsensi.models.Absen
 import com.tugas.platform.aplikasiabsensi.utils.Constants
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 class AbsensAdapter(val data: List<Absen>?, private val context: Context) : RecyclerView.Adapter<AbsensAdapter.MyHolder>() {
@@ -29,6 +33,31 @@ class AbsensAdapter(val data: List<Absen>?, private val context: Context) : Recy
             binding.tvJam.text = data?.get(position)?.jam?.let { it -> sourceFormat.parse(it)?.let { jamFormat.format(it) } }
             binding.tvLokasi.text = data?.get(position)?.lokasi
             Glide.with(context).load(Constants.ABSEN_IMG_URL+data?.get(position)?.foto).into(binding.ivAbsen)
+
+            val time: Date? = data?.get(position)?.jam?.let { it -> sourceFormat.parse(it)?.let { jamFormat.format(it) } }
+                ?.let { it1 -> jamFormat.parse(it1) }
+
+            val waktuMasuk: Date? = jamFormat.parse("07:30:00")
+            val waktuPulang: Date? = jamFormat.parse("15:30:00")
+
+            if (time != null) {
+                if (data?.get(position)?.jenis == "Absen Masuk") {
+                    if (time.compareTo(waktuMasuk) == 0 || time.compareTo(waktuMasuk) < 0) {
+                        binding.icStatus.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.green))
+                    } else {
+                        binding.icStatus.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.red))
+
+                    }
+                }
+
+                if (data?.get(position)?.jenis == "Absen Pulang") {
+                    if (time.compareTo(waktuPulang) == 0 || time.compareTo(waktuPulang) > 0) {
+                        binding.icStatus.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.green))
+                    } else {
+                        binding.icStatus.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.grey))
+                    }
+                }
+            }
         }
     }
 
