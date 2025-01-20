02ec01d990394ce6f4900ad36587d2755c2c36a1
Yabesmaych <yabesmaych@gmail.com>
2025-01-20 12:02:59 +0700

    FIX

--- app/src/main/java/com/tugas/platform/aplikasiabsensi/utils/Constants.kt
+++ app/src/main/java/com/tugas/platform/aplikasiabsensi/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.tugas.platform.aplikasiabsensi.utils
 object Constants {
 
     // Endpoints
-    const val HOST = "http://172.20.10.6:8000"
+    const val HOST = "http://192.168.0.134:8000"
     const val BASE_URL = HOST + "/api/"
     const val ABSEN_IMG_URL = HOST + "/storage/absen/"
     const val PROFILE_IMG_URL = HOST + "/storage/user/"
